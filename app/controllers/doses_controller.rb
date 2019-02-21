9dbaf93f0fa104cf77015e6516a9c37f38968b1f
Filippo Tognacci <fitog4@hotmail.it>
2019-02-21 15:20:37 +0100

    implement cocktail actions and form to add an ingredient to a cocktail from cocktails/show

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,19 @@
+class DosesController < ApplicationController
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.valid?
+      @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'cocktails/show'
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
+end
